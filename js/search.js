ddf396dcb7d694d66156d57b6032ac7f24f534d2
YasukazuMakino <mkn@yskz.dev>
2023-02-13 03:47:03 +0900

    momentally

--- js/search.js
+++ js/search.js
@@ -1,99 +1,122 @@
-'use strict';
-
-var searchFunc = function(path, search_id, content_id) {
-  $.ajax({
-    url: path,
-    dataType: "xml",
-    success: function( xmlResponse ) {
-      // get the contents from search data
-      var datas = $( "entry", xmlResponse ).map(function() {
-          return {
-              title: $( "title", this ).text(),
-              content: $("content",this).text(),
-              url: $( "url" , this).text()
-          };
-      }).get();
-      var $input = document.getElementById(search_id);
-      if (!$input) return;
-      var $resultContent = document.getElementById(content_id);
-      $input.addEventListener('input', function(){
-        var str='<ul class=\"search-result-list\">';
-        var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-        $resultContent.innerHTML = "";
-        if (this.value.trim().length <= 0) {
-            return;
+function fetchData (fetchUrl) {
+  return new Promise(resolve => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', fetchUrl, true)
+    xhr.responseType = 'document'
+    xhr.overrideMimeType('text/xml')
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 200 || xhr.status === 304) {
+          resolve(xhr.response)
         }
-        // perform local searching
-        var numOfPostFound = 0; // keeping track of # of result
-        datas.forEach(function(data) {
-          var isMatch = true;
-          var content_index = [];
-          var data_title = data.title.trim().toLowerCase();
-          var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
-          var data_url = data.url;
-          var index_title = -1;
-          var index_content = -1;
-          var first_occur = -1;
-          // only match artiles with not empty titles and contents
-          if(data_title != '' && data_content != '') {
-              keywords.forEach(function(keyword, i) {
-                index_title = data_title.indexOf(keyword);
-                index_content = data_content.indexOf(keyword);
-                if( index_title < 0 && index_content < 0 ){
-                  isMatch = false;
-                } else {
-                  if (index_content < 0) {
-                    index_content = 0;
-                  }
-                  if (i == 0) {
-                    first_occur = index_content;
-                  }
-                }
-              });
-          }
-          // show search results
-          if (isMatch) {
-            numOfPostFound += 1; // keeping track of # of results
-            str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a><hr>";
-            var content = data.content.trim().replace(/<[^>]+>/g,"");
-            if (first_occur >= 0) {
-              // cut out 100 characters
-              var start = first_occur - 20;
-              var end = first_occur + 80;
-              if(start < 0){
-                start = 0;
-              }
-              if(start == 0){
-                end = 100;
-              }
-              if(end > content.length){
-                end = content.length;
-              }
-              var match_content = content.substr(start, end);
-              // highlight all keywords
-              keywords.forEach(function(keyword){
-                var regS = new RegExp(keyword, "gi");
-                match_content = match_content.replace(regS, "<em class=\"search-keyword\">"+keyword+"</em>");
-              });
-              str += "<p class=\"search-result\">" + match_content +"...</p>"
-            }
-            str += "</li><br>";
-          }
-        });
-        str += "</ul>";
-        // attaching a summary of searching result
-        if (numOfPostFound > 0) {
-          if (numOfPostFound > 1) {
-            summary = numOfPostFound + " 件見つかりました。";
-          } else {
-            summary = numOfPostFound + " 件見つかりました。";
-          }
-        } else {
-          summary = "見つかりませんでした。";
-        }
-        var summary = "<p class=\"text-xlarge text-color-base archieve-result search-result-summary\">" + summary + "</ul><br><br>";
-        $resultContent.innerHTML = summary + str;
-      });
+      }
     }
-  });
+    xhr.send(null)
+  })
 }
+
+function analyzeData(document, query) {
+  const entries = document.getElementsByTagName('entry')
+  const matchEntries = []
+  for (var entry of entries) {
+    const regExp = new RegExp(query)
+    if (regExp.test(entry.children[0].textContent) ||
+        regExp.test(entry.children[2].textContent)) {
+      matchEntries.push(entry)
+    }
+  }
+  return matchEntries
+}
+
+function makeSearchResult (entries) {
+  let innerHTML = ''
+  for (let entry of entries) {
+    innerHTML += '<div class="search-result-entry">'
+    const title = entry.children[0].textContent
+    const url = entry.children[3].textContent
+    const content = entry.children[3].textContent
+    innerHTML += '<h2><a href="' + url + '">' + title + '</a></h2>'
+    const thumbnail = /<img[^>]*>/.exec(content)
+    if (thumbnail && thumbnail.length >= 1) {
+      innerHTML += '<div class="search-result-thumbnail">' + thumbnail[1] + '</div>'
+    }
+    innerHTML += content.replace(/<[^>]*>/g, '').substring(0, 300)
+    innerHTML += '...</div>'
+  }
+  return innerHTML
+}
+
+function getSearchQueryFromUrlParams () {
+  const params = window.location.search.substring(1).split('&');
+  const search = params.filter(param => param.search(/$search=/));
+  return search.length > 0 ? search[0].split('=')[1] : null;
+}
+
+const template_search_result_str = "template#search-result";
+let searchResultStr = "search-result";
+let form = document.querySelector("form#search");
+if (!form) {
+  console.log("'form#search' is not found.");
+}
+
+const search_text_tag = "input#search-text";
+function search() {
+  const searchResult = document.querySelector(template_search_result_str);
+  if (!searchResult) {
+    console.error(template_search_result_str + " is not found!");
+    return false;
+  }
+  const search_text = document.querySelector(search_text_tag);
+  if (!search_text) {
+    console.error(search_text_tag + " is not found.");
+    return false;
+  }
+  if (search_text.value.length <= 0) {
+    console.error("search text length is 0!");
+    return false;
+  }
+  const queryWord = search_text.value;
+  const fetch_path = '/search.xml';
+  searchResult.textContent = `FetchData from ${fetch_path} with ${queryWord}`;
+  fetchData('/search.xml').then(document => {
+    const entries = analyzeData(document, queryWord); 
+    if (entries.length <= 0) {
+      console.log("entries.length is zero.");
+    }
+    debugger;
+    searchResult.innerHTML = makeSearchResult(entries)
+  })
+  return true;
+}
+
+// Replace serchResultStr to 'plugin-search-result' if '#search-result' not found.
+function init_search() {
+  let searchResult = document.querySelector(`#${searchResultStr}`);
+  if (!searchResult) {
+    const url_prefix = "plugin-";
+    console.log(`#${searchResultStr} is not found.`);
+    searchResultStr = url_prefix + searchResultStr;
+    console.log(`Fell back to #${searchResultStr}.`);
+    searchResult = document.querySelector(`#${searchResultStr}`);
+    if (!searchResult) {
+      console.error(searchResultStr + " is not found!");
+      return false;
+    }
+    searchResult.textContent = 'Loading...'
+    fetchData('/search.xml').then(document => {
+      const queryWord = getSearchQueryFromUrlParams();
+      if (queryWord == null) {
+        console.log("Query word is null.");
+        return;
+      }
+      console.log(`Query word is ${queryWord}.`);
+      const entries = analyzeData(document, queryWord); 
+      searchResult.innerHTML = makeSearchResult(entries)
+    })
+    return true;
+  }
+  return true;
+}
+
+if (!init_search())
+  console.error("init_search() call failed!");
