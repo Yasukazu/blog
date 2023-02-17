868e0e4f491bf1cc5fa022d478449643838863e3
YasukazuMakino <mkn@yskz.dev>
2023-02-17 16:06:31 +0900

    search result codepoint 299

--- js/search.js
+++ js/search.js
@@ -27,69 +27,83 @@ function fetchData (fetchUrl) {
  * 
  * @param {Document} document // XML
  * @param {string} query_str // Regex expression
- * @returns {Array<Element>}
+ * @returns {Object< Array<Element>, Array<string> >}
  */
 function analyzeData(document, query_str) {
   const entries = document.getElementsByTagName('entry');
   const matchEntries = [];
+  const matchItems = [];
   const normalized_query = query_str.normalize('NFKD');
   const combining_chars_regex = /\p{Mark}/gu;
   const query = normalized_query.replace(combining_chars_regex, '');
   const query_regex = RegExp(query);
   const test_children = [0, 2];
-  for (var entry of entries) {
+  const test_items = ['title', 'content'];
+  for (let entry of entries) {
     let match = false;
     let content = '';
-    for (var cn of test_children) {
-      let _content = entry.children[cn]?.textContent;
-      if (_content)
-        content = _content.normalize('NFKD').replace(combining_chars_regex, "");
-      if (query_regex.test(content)) {
-        match = true;
-        break;
+    let item = '';
+    for (item of test_items) { // for (let cn of test_children) {
+      const elements = entry.querySelectorAll(item);
+      if (elements) {
+        for (let elem of elements) {
+          let text = elem.textContent;
+          if (text) {
+            content = text.normalize('NFKD').replace(combining_chars_regex, "");
+            if (content && query_regex.test(content)) {
+              match = true;
+              break;
+            }
+          }
+        }
       }
     }
     if (match)
       matchEntries.push(entry);
+      matchItems.push(item);
   }
-  return matchEntries;
+  return {'entries': matchEntries, 'items': matchItems};
 }
 
 /**
  * 
- * @param {Array<Element>} entries 
+ * @param {Array<Element>} entries
+ * @param {Array<string>} items 
  * @returns {Element}
  */
-function makeSearchResultFromTemplates (entries) {
+function makeSearchResultFromTemplates (entries, items) {
   let template_str = "template#search-result-container";
   let template = document.querySelector(template_str);
   if (!template) {
-    throw `${template_str} is not found!`;
+    throw `'${template_str}' is not found!`;
   }
   const search_result_container = document.importNode(template.content, true);
   const search_result_entries = search_result_container.querySelector('.entries');
   if (!search_result_entries) {
-    throw `.entries is not found!`;
+    throw `.entries is not found in search result container!`;
   }
 
   template_str = "template#search-result-entry";
   template = document.querySelector(template_str);
   if (!template) {
-    throw `${template_str} is not found!`;
+    throw `'${template_str}' template is not found!`;
   }
-  for (let entry of entries) {
+  for (const [i, entry] of entries.entries()) {
     const entry_output = document.importNode(template.content, true);
-    const title = entry.children[0].textContent
+    const title = entry.querySelector('title')?.textContent; // children[0]
     if (!title) {
-      throw "No title!";
+      throw "No title in entry!";
     }
-    const url = entry.children[2].textContent
+    const url = entry.querySelector('url')?.textContent; // 2
     if (!url) {
-      throw "No url!";
+      throw "No 'url' in entry!";
     }
 
     // const tags = entry.children[4].textContent
     const ar = entry_output.querySelector('a.title');
+    if (!ar) {
+      throw "No 'a' in template!"
+    }
     ar.href = url;
     ar.innerText = title;
     // pick up date from url beginning
@@ -103,17 +117,46 @@ function makeSearchResultFromTemplates (entries) {
 
     const ct = entry_output.querySelector('.content');
     if (ct){
-      const content = entry.children[3].textContent;
-      const content_tree = content ? new DOMParser().parseFromString(content, "text/html") : null;
-      const text_content = content_tree?.children[0]?.textContent;
-      if (text_content) {
-        // ct.innerText = content.replace(/<[^>]*>/g, '').substring(0, 300) + '...';
-        ct.innerText = text_content.substring(0, 300) + '...';
+      const content = entry.querySelector('content')?.textContent;
+      if (content) {
+        const content_tree = new DOMParser().parseFromString(content, "text/html");
+        const text_content = content_tree?.children[0]?.textContent;
+        if (text_content) {
+          // ct.innerText = content.replace(/<[^>]*>/g, '').substring(0, 300) + '...';
+          const chars = Array.from(text_content); // code point sequences
+          const limitedStr = getFirstNChars(chars, 300);
+          // const innerText = chars.slice(0, 300).join('').replace(/\s+/g, ' ');
+          ct.innerText = limitedStr;
+        }
       }
     }
     search_result_entries.append(entry_output);
   }
   return search_result_container;
+}
+
+/**
+ * 
+ * @param {string[]} src 
+ * @param {Number} n 
+ * @returns {string}
+ */
+function getFirstNChars(src, n) {
+  let lc = '';
+  let i = 0;
+  let out = '';
+  for (let c of src) {
+    if (lc === ' ' && lc === c) {
+      continue;
+    }
+    else {
+      lc = c;
+    }
+    out += c;
+    if(++i >= n)
+      break;
+  }
+  return out;
 }
 
 /**
@@ -172,14 +215,14 @@ function search() {
   }
   // let search_result = `FetchData from ${fetch_path} with ${queryWord}`;
   fetch_data.then(document => {
-    const entries = analyzeData(document, queryWord); 
+    const {entries, items} = analyzeData(document, queryWord); 
     if (entries.length <= 0) {
       console.log("entries.length is zero.");
     }
     while (searchResult.firstChild) {
       searchResult.firstChild.remove();
     }
-    const search_result = makeSearchResultFromTemplates(entries)
+    const search_result = makeSearchResultFromTemplates(entries, items);
     if (search_result) {
       searchResult.append(search_result);
     }
